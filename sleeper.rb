2701211cd0f76827a65eca8335979d18a15a46fd
KD <whomst@kevind.xyz>
2023-12-26 17:30:12 +0100

    chore: update tar path in formula

--- sleeper.rb
+++ sleeper.rb
@@ -4,10 +4,7 @@ class Sleeper < Formula
   url "https://github.com/thekidnamedkd/sleeper/releases/download/v1.0.0/sleeper-v1.0.0.tar.gz"
   sha256 "f14c26dc37c13d4b01b01ae93fadc18759e5774c6fa32e7fb2554a7d421d42ed"
 
-  bottle :unneeded
-
   def install
-    # Unpack the tarball
     bin.install "sleeper_config.sh"
     bin.install "sleeper_loop.sh"
     bin.install "sleeper_execute.scpt"
@@ -15,12 +12,10 @@ class Sleeper < Formula
     doc.install "README.md"
   end
 
-  def caveats
-    <<~EOS
-      To configure Sleeper, run the configuration script in the installation directory:
-      cd #{var.homebrew_prefix}/bin
-      ./sleeper_config.sh
-    EOS
+  def caveats; <<~EOS
+    To configure Sleeper, run the configuration script in the installation directory:
+    cd #{var.homebrew_prefix}/bin
+    ./sleeper_config.sh
+  EOS
   end
-
 end
