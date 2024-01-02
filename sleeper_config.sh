4e15ca459c28066a79a1c8a84111ca69d4d817ab
KD <whomst@kevind.xyz>
2024-01-02 17:22:01 +0100

    fix: update config and formula for pathing loop

--- sleeper_config.sh
+++ sleeper_config.sh
@@ -1,34 +1,30 @@
-#!/bin/bash
-
 echo "Configuring Sleeper..."
 
-# Ask for idle time
 read -p "Enter the desired idle time in seconds (default 900): " idle_time
 idle_time=${idle_time:-900}
 
-# Ask for a custom username for the label
 read -p "Enter a custom username for the label (default 'user'): " custom_username
 custom_username=${custom_username:-user}
 
-# Define the path to the plist file in the Homebrew cellar
 plist_cellar_path="$(brew --prefix sleeper)/libexec/sleeper_launch.plist"
 
-# Check if the plist file exists in the cellar
+sleeper_loop_path="$(brew --prefix sleeper)/bin/sleeper_loop.sh"
+
 if [ ! -f "$plist_cellar_path" ]; then
     echo "Error: plist file not found at $plist_cellar_path"
     exit 1
 fi
 
-# Update the idle time in the plist file
+/usr/libexec/PlistBuddy -c "Set :ProgramArguments:0 $sleeper_loop_path" "$plist_cellar_path"
+echo "Updated path in plist to $sleeper_loop_path."
+
 /usr/libexec/PlistBuddy -c "Set :EnvironmentVariables:IDLE_TIME $idle_time" "$plist_cellar_path"
 echo "Updated idle time in plist to $idle_time seconds."
 
-# Update the label in the plist file
 label="com.$custom_username.sleeper"
 /usr/libexec/PlistBuddy -c "Set :Label $label" "$plist_cellar_path"
 echo "Updated label in plist to $label."
 
-# Link the plist file to the user's LaunchAgents directory
 launch_agents_dir="$HOME/Library/LaunchAgents"
 mkdir -p "$launch_agents_dir"
 ln -sfv "$plist_cellar_path" "$launch_agents_dir/$label.plist"
