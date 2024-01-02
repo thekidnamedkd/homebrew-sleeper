4b04b269af93d4e0b1630a681da2f8fda91b9ae7
KD <whomst@kevind.xyz>
2024-01-02 12:29:19 +0100

    chore: update recipe for v1.0.5

--- sleeper.rb
+++ sleeper.rb
@@ -1,7 +1,7 @@
 class Sleeper < Formula
   desc "A macOS utility for managing computer sleep behavior"
   homepage "https://github.com/thekidnamedkd/homebrew-sleeper"
-  url "https://github.com/thekidnamedkd/homebrew-sleeper/raw/v1.0.4x/sleeper-app.tar.gz"
+  url "https://github.com/thekidnamedkd/homebrew-sleeper/raw/v1.0.5/sleeper-app.tar.gz"
   sha256 "a5220efad960ca175c84f0fba45ead8d01bbdaf4a30ab87a71fb34d258c71fd1"
 
   def install
@@ -9,6 +9,7 @@ class Sleeper < Formula
     bin.install "sleeper_loop.sh"
     bin.install "sleeper_execute.scpt"
     inreplace "sleeper_launch.plist", "/path/to/sleeper_loop.sh", "#{bin}/sleeper_loop.sh"
+    
     libexec.install "sleeper_launch.plist"
   end
 
