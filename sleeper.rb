84ad7da8172a7ef294ac248b8a0490b71b2de0b9
KD <whomst@kevind.xyz>
2024-01-02 17:33:41 +0100

    fix: move to postinstall permissioning

--- sleeper.rb
+++ sleeper.rb
@@ -1,15 +1,18 @@
 class Sleeper < Formula
   desc "A macOS utility for managing computer sleep behavior"
   homepage "https://github.com/thekidnamedkd/homebrew-sleeper"
-  url "https://github.com/thekidnamedkd/homebrew-sleeper/raw/v1.0.6/sleeper-app.tar.gz"
-  sha256 "0e96c5b066508e062edc4a31822d2cadf4c52de697b10aba5e1a9e9b977d5906"
+  url "https://github.com/thekidnamedkd/homebrew-sleeper/raw/v1.0.5/sleeper-app.tar.gz"
+  sha256 "a5220efad960ca175c84f0fba45ead8d01bbdaf4a30ab87a71fb34d258c71fd1"
 
   def install
     bin.install "sleeper_config.sh"
     bin.install "sleeper_loop.sh"
     bin.install "sleeper_execute.scpt"
     libexec.install "sleeper_launch.plist"
+  end
 
+  def post_install
+    # Set execute permissions after installation
     chmod 0755, bin/"sleeper_config.sh"
     chmod 0755, bin/"sleeper_loop.sh"
   end
@@ -18,7 +21,6 @@ class Sleeper < Formula
     To configure Sleeper, run the configuration script in the installation directory:
     cd $(brew --prefix)/bin
     ./sleeper_config.sh
-    EOS
+  EOS
   end
-
 end
